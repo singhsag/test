df7a154d9d92059e29350452b4e37696cbc5f418
Sagar <59777833+singhsag@users.noreply.github.com>
2024-09-13 14:50:26 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-export function App(props) {
+function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
@@ -44,3 +44,5 @@ export function App(props) {
     </div>
   );
 };
+
+export default App;
